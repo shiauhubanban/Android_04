f1dd38b9cea26e2d7b3f00c8a18472ffc505eec1
Liao <unidein@gmail.com>
2016-11-16 10:51:46 +0800

    v3

--- app/src/main/java/tw/org/iii/shine04/MainActivity.java
+++ app/src/main/java/tw/org/iii/shine04/MainActivity.java
@@ -1,60 +1,72 @@
 package tw.org.iii.shine04;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.util.Log;
-
-import static tw.org.iii.shine04.R.styleable.View;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button test1, test2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v("shine","onCreate");
+        Log.v("brad","onCreate");
+
+        test1 = (Button)findViewById(R.id.test1);
+        test2 = (Button)findViewById(R.id.test2);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.v("shine","onStart");
+        Log.v("brad","onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.v("shine","onResume");
+        Log.v("brad","onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.v("shine","onPause");
+        Log.v("brad","onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.v("shine","onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.v("shine","onDestroy");
+        Log.v("brad","onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.v("shine","onRestart");
+        Log.v("brad","onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("brad","onDestroy");
     }
 
     public void test1(View view){
-
-
+        if (view == test1) {
+            Intent it = new Intent(this, Page2Activity.class);
+            startActivity(it);
+            Log.v("brad", "test1 return");
+        }else if (view == test2){
+            finish();
+        }
     }
+
+
 
 }
