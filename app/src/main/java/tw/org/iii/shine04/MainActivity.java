4ffec7c38f7e2745e8c5a5d9c52ac089e2e41382
Liao <unidein@gmail.com>
2016-11-16 10:05:54 +0800

    v2

--- app/src/main/java/tw/org/iii/shine04/MainActivity.java
+++ app/src/main/java/tw/org/iii/shine04/MainActivity.java
@@ -2,6 +2,10 @@ package tw.org.iii.shine04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+
+import static tw.org.iii.shine04.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("shine","onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("shine","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("shine","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("shine","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("shine","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("shine","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("shine","onRestart");
+    }
+
+    public void test1(View view){
+
+
+    }
+
 }
